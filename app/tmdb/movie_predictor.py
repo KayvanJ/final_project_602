a0c35a0d489d40a58e4527185d5893e1537af024
psweet <49662397+psweet@users.noreply.github.com>
2022-12-13 11:21:27 -0500

    More memory enhancements

--- app/tmdb/movie_predictor.py
+++ app/tmdb/movie_predictor.py
@@ -12,7 +12,6 @@ import json
 class MovieModel:
 
     def __init__(self):
-        self.dataframe = None
         self.model = None
         self.score = 0
         self.mse = 0
@@ -20,14 +19,11 @@ class MovieModel:
 
         self.ready = False
 
-        self.open_db()
-        self.clean_data()
-        self.train_model()
-
+        self.start()
         print(
             f"\n##############\n\nReady with score {self.score} and error of {self.mse}\n\n##############\n")
 
-    def open_db(self):
+    def start(self):
         if not self.ready:
             print("Loading data")
 
@@ -40,42 +36,17 @@ class MovieModel:
                 db = db + data
                 f.close()
                 print(num)
+        print("Movies to train on ", len(db))
+        db = pd.DataFrame.from_dict(db)
 
-        self.dataframe = pd.DataFrame.from_dict(db)
-
-    def clean_data(self):
-        if not self.ready:
-            print("Cleaning")
-
-            self.dataframe = self.dataframe[self.dataframe["revenue"] != 0]
-            self.dataframe = self.dataframe[self.dataframe["status"]
-                                            == "Released"]
-
-            avg_budget = self.dataframe['budget'].mean()
-            std_budget = self.dataframe['budget'].std()
-            avg_revenue = self.dataframe['revenue'].mean()
-            std_revenue = self.dataframe['revenue'].std()
-
-            self.dataframe['centered_budget'] = self.dataframe['budget'].apply(
-                lambda x: (x-avg_budget)/std_budget)
-            self.dataframe['centered_revenue'] = self.dataframe['revenue'].apply(
-                lambda x: (x-avg_revenue)/std_revenue)
-            self.dataframe['anomaly'] = self.dataframe['revenue'].apply(
-                lambda x: 1 if x > (72287747*2) else 0)
-
-            self.dataframe.dropna(subset=['revenue/day'], inplace=True)
-
-    def train_model(self):
         if not self.ready:
             print("Training")
 
-            X = np.array([self.dataframe['centered_budget'], self.dataframe['vote_count'],
-                         self.dataframe['popularity'], self.dataframe['anomaly']]).T
-
-            y = np.array(self.dataframe['centered_revenue']).reshape(-1, 1)
-
             X_train, X_test, y_train, y_test = train_test_split(
-                X, y, test_size=0.2, random_state=101)
+                np.array([db['centered_budget'], db['vote_count'],
+                         db['popularity'], db['anomaly']]).T,
+                np.array(db['centered_revenue']).reshape(-1, 1),
+                test_size=0.2, random_state=101)
 
             model = GradientBoostingRegressor(
                 n_estimators=1000,
@@ -88,16 +59,14 @@ class MovieModel:
             model.fit(X_train, y_train)
             y_pred = model.predict(X_test)
 
+            self.revenue_std = db['revenue'].std()
+            self.revenue_avg = db['revenue'].mean()
             self.model = model
             self.score = model.score(X_test, y_test)
             self.mse = mean_squared_error(y_true=y_test, y_pred=y_pred)
             self.ready = True
 
     def predict(self, features):
-        final_features = features
-        final_number = (
-            self.model.predict(final_features)[
-                0] * self.dataframe['revenue'].std()
-        ) + self.dataframe['revenue'].mean()
-
+        pred = self.model.predict(features)[0]
+        final_number = (pred * self.revenue_std) + self.revenue_avg
         return round(final_number)
